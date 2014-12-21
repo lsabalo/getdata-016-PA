7eeda351cefafd69fe6fb4643f444a6cde1935d8
Lucas Sabalo <lsabalo@gmail.com>
2014-12-21 08:57:55 -0700

    Project Assignment
    Initial version

---
+++ CodeBook.md
@@ -0,0 +1,12 @@
+Data requirements
+
+1) Download file https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+2) Uncompress and move it to the same directory as run_analysis.R script. It should look like this:
+
+run_analysis.R
+UCI HAR Dataset/activity_labels.txt
+UCI HAR Dataset/features.txt
+UCI HAR Dataset/Features_info.txt
+...
+...
+
---
+++ run_analysis.R
@@ -0,0 +1,42 @@
+# Returns a data frame with all files in a single data set
+mergedata <- function() {
+    
+    # features.txt has a description of each column
+    features <- read.table("UCI HAR Dataset//features.txt")
+
+    # Extract the number of the columns we were requested (mean and std)
+    columns <- grep("[mM]ean|[sS]td", features[,2])
+
+    xtest <- read.table("UCI HAR Dataset/test/X_test.txt")
+    ytest <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "activityid")
+    xtrain <- read.table("UCI HAR Dataset/train/X_train.txt")
+    ytrain <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "activityid")    
+    
+    # Pull only columns we are interested on
+    xtest <- xtest[, columns]
+    xtrain <- xtrain[, columns]
+    
+    # Assig names to columns
+    names(xtest) <- tolower(gsub("[(|)|,|-]", "", features[columns, 2]))
+    names(xtrain) <- tolower(gsub("[(|)|,|-]", "", features[columns, 2]))
+
+    # activity_labels.txt has the name of each activity
+    activitylabels <- read.table("UCI HAR Dataset//activity_labels.txt", sep=" ", col.names = c("activityid", "activity"))
+    
+    # Merge of the activity recored with its label
+    ytestlabeled <- merge(ytest, activitylabels, by="activityid", sort=FALSE, all.x=TRUE)
+    ytrainlabeled <- merge(ytrain, activitylabels, by="activityid", sort=FALSE, all.x=TRUE)
+
+    subjecttest <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "personid")
+    subjecttrain <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "personid")
+    
+    test <- cbind(subjecttest[1], ytestlabeled[2], xtest)
+    train <- cbind(subjecttrain[1], ytrainlabeled[2], xtrain)
+    
+    rbind(test, train)
+}
+
+avgdata <- function(x) {
+    tmp <- aggregate(. ~ personid + activity, data, mean)
+    tmp[order(tmp$personid, tmp$activity), ]
+}
